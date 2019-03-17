363c7e5239aa52ae1c7e234fba24df48021ffe35
Lin <chinaherolining@163.com>
2019-03-17 10:51:33 +0800

    10.首页_FlutterSwiper轮播效果制作

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,27 +1,55 @@
 import 'package:flutter/material.dart';
 import '../service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
 
 class HomePage extends StatefulWidget{
   _HomePageState createState() => _HomePageState();
 }
 class _HomePageState extends State<HomePage>{
   String homePageContent ='正在获取数据';
-  @override
-  void initState(){
-    getHomePageContent().then((val){
-      homePageContent=val.toString();
-    });
-    super.initState();
-  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title:Text('百姓生活+')),
-      body: SingleChildScrollView(
-        child: Text(homePageContent),
-      ),
+      body: FutureBuilder(
+          future: getHomePageContent(),
+          builder: (context,snapshot){
+            if(snapshot.hasData){
+              var data = json.decode(snapshot.data.toString());
+              List<Map> swiper = (data['data']['slides'] as List).cast();
+              return Column(
+                children: <Widget>[SwiperDiy(swiperDateList: swiper)],
+              );
+            }else{
+              return Center(
+                child: Text('加载中。。。。。'),
+              );
+            }
+          }),
     );
   }
 
 
+}
+//首页轮播组件
+class SwiperDiy extends StatelessWidget {
+  final List swiperDateList;
+  SwiperDiy({Key key,this.swiperDateList});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 333,
+      child: Swiper(
+        itemBuilder: (BuildContext context,int index){
+          return Image.network("${swiperDateList[index]['image']}");
+        },
+        itemCount: 3,
+        pagination: SwiperPagination(),
+        autoplay: true,
+      ),
+    );
+  }
 }
