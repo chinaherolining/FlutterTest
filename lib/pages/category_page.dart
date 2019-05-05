ccf4bb3b604482ad2d8a5793d5348359c5ca9a08
Lin <chinaherolining@163.com>
2019-05-05 21:15:17 +0800

    25

--- lib/pages/category_page.dart
+++ lib/pages/category_page.dart
@@ -16,7 +16,12 @@ class _CategoryPageState extends State<CategoryPage> {
       body: Container(
         child: Row(
           children: <Widget>[
-            LeftCatgegoryNav()
+            LeftCatgegoryNav(),
+            Column(
+              children: <Widget>[
+                RightCategoryNav(),
+              ],
+            )
           ],
         ),
       ),
@@ -86,5 +91,49 @@ class _LeftCatgegoryNavState extends State<LeftCatgegoryNav> {
 
 //      print(data);
     });
+  }
+}
+
+class RightCategoryNav extends StatefulWidget {
+  @override
+  _RightCategoryNavState createState() => _RightCategoryNavState();
+}
+
+class _RightCategoryNavState extends State<RightCategoryNav> {
+  List  list = ['名酒','宝丰','北京二锅头','五粮液','茅台','散白'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: ScreenUtil().setHeight(80),
+      width: ScreenUtil().setWidth(540),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1,color: Colors.black12)
+        )
+      ),
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemCount: list.length,
+          itemBuilder: (context,index){
+            return _rightInkWell(list[index]);
+          }),
+    );
+  }
+
+  Widget _rightInkWell(String item){
+    return InkWell(
+      onTap: (){
+
+      },
+      child: Container(
+        padding: EdgeInsets.fromLTRB(5.0, 10.0, 5.0, 10.0),
+        child: Text(
+          item,
+          style: TextStyle(fontSize: ScreenUtil().setSp(28)),
+        ),
+      ),
+    );
   }
 }
