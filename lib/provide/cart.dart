c8d47858efd9ba8a2ee725a6b8f2dcf6b2c21076
Lin <chinaherolining@163.com>
2019-05-18 18:46:59 +0800

    51

--- lib/provide/cart.dart
+++ lib/provide/cart.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:convert';
+import '../model/cartInfo.dart';
 class CartProvide with ChangeNotifier{
   String cartString = "[]";
+  List<CartInfoModel> cartList = [];
   save(goodsId,goodsName,count,price,images) async{
     SharedPreferences prefs = await SharedPreferences.getInstance();
     cartString = prefs.getString('cartInfo');
@@ -13,22 +15,26 @@ class CartProvide with ChangeNotifier{
     tempList.forEach((item){
       if(item['goodsId'] == goodsId){
         tempList[ival]['count'] = item['count']+1;
+        cartList[ival].count++;
         isHave = true;
       }
       ival++;
     });
     if(!isHave){
-      tempList.add({
+      Map<String,dynamic> newGoods ={
         'goodsId':goodsId,
         'goodsName':goodsName,
         'count':count,
         'price':price,
         'images':images
-      });
+      };
+      tempList.add(newGoods);
+      cartList.add(CartInfoModel.fromJson(newGoods));
     }
 
     cartString = json.encode(tempList).toString();
-    print(cartString);
+    print('字符串》》》》》》》》${cartString}');
+    print('数据模型》》》》》》》》${cartList}');
     prefs.setString('cartInfo', cartString);
     notifyListeners();
   }
@@ -36,7 +42,23 @@ class CartProvide with ChangeNotifier{
   remove() async{
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.remove('cartInfo');
+    cartList = [];
     print('清空完成。。。。。');
+    notifyListeners();
+  }
+
+  getCartInfo() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    cartString = prefs.getString('cartInfo');
+    cartList = [];
+    if(cartString==null){
+      cartList=[];
+    }else{
+      List<Map> tempList = (json.decode(cartString.toString()) as List).cast();
+      tempList.forEach((item){
+        cartList.add(CartInfoModel.fromJson(item));
+      });
+    }
     notifyListeners();
   }
 }
