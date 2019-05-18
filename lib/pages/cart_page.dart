68816b84d441f91494a7e8446f33e4fe0964fefa
Lin <chinaherolining@163.com>
2019-05-18 12:40:44 +0800

    50

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,49 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:provide/provide.dart';
 import '../provide/counter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class CartPage extends StatelessWidget{
+class CartPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-        body:Center(
-          child: Column(
-            children: <Widget>[
-              Number(),
-              MyButton()
-            ],
+  _CartPageState createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  List<String> testList = [];
+  @override
+  Widget build(BuildContext context) {
+    _show();
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            height: 400.0,
+            child: ListView.builder(
+                itemCount: testList.length,
+                itemBuilder: (context,index){
+                  return ListTile(
+                    title: Text(testList[index]),
+                  );
+                }),
+          ),
+          RaisedButton(
+            onPressed: (){
+              _add();
+            },
+            child: Text('增加'),
+          ),
+          RaisedButton(
+            onPressed: (){
+              _clear();
+            },
+            child: Text('清空'),
           )
-        )
-    );
-  }
-}
-
-class Number extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top:200,),
-      child: Provide<Counter>(
-          builder: (context,child,counter){
-        return Text('${counter.value}',style: Theme.of(context).textTheme.display1,);
-      }),
-    );
-  }
-}
-
-class MyButton extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: RaisedButton(
-        onPressed: (){
-          Provide.value<Counter>(context).increment();
-        },
-        child: Text('递增'),
+        ],
       ),
-
     );
   }
+  //增加方法
+  void _add() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String temp ="苹果醉落";
+    testList.add(temp);
+    prefs.setStringList("testInfo", testList);
+    _show();
+  }
+  //查询方法
+  void _show() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    if(prefs.getStringList('testInfo') != null){
+      setState((){
+        testList = prefs.getStringList('testInfo');
+      });
+    }
+
+  }
+
+  //删除方法
+  void _clear() async{
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.remove('testInfo');
+    setState((){
+      testList = [];
+    });
+
+  }
 }
+
+
+
+//class CartPage extends StatefulWidget {
+//  @override
+//  _CartPageState createState() => _CartPageState();
+//}
+//
+//class _CartPageState extends State<CartPage> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Container();
+//  }
+//}
+
+
+
 
