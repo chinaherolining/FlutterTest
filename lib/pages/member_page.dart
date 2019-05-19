78874062f544ad6d20bd9b1ae8d9fbc787e58a54
Lin <chinaherolining@163.com>
2019-05-19 19:39:44 +0800

    65

--- lib/pages/member_page.dart
+++ lib/pages/member_page.dart
@@ -13,6 +13,8 @@ class MemberPage extends StatelessWidget{
         body:ListView(
           children: <Widget>[
             _topHeader(),
+            _orderTitle(),
+            _orderType()
           ],
         )
     );
@@ -43,6 +45,99 @@ class MemberPage extends StatelessWidget{
                 ),
             ),
           )
+        ],
+      ),
+    );
+  }
+
+  //我的订单
+  Widget _orderTitle(){
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1,color: Colors.black12),
+        )
+      ),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title: Text('我的订单'),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
+  //
+  Widget _orderType(){
+    return Container(
+      margin: EdgeInsets.only(top: 5),
+      width: ScreenUtil().setWidth(700),
+      height: ScreenUtil().setHeight(150),
+      padding: EdgeInsets.only(top: 20),
+      color: Colors.white,
+      child: Row(
+        children: <Widget>[
+          Container(
+            width: ScreenUtil().setWidth(180),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.party_mode,
+                  size: 30,
+
+                ),
+                Text('待付款')
+
+              ],
+            ),
+          ),
+          //-------
+          Container(
+            width: ScreenUtil().setWidth(180),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.query_builder,
+                  size: 30,
+
+                ),
+                Text('待发货')
+
+              ],
+            ),
+          ),
+          //------
+          Container(
+            width: ScreenUtil().setWidth(180),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.directions_car,
+                  size: 30,
+
+                ),
+                Text('待收货')
+
+              ],
+            ),
+          ),
+          //------
+          Container(
+            width: ScreenUtil().setWidth(180),
+            child: Column(
+              children: <Widget>[
+                Icon(
+                  Icons.pageview,
+                  size: 30,
+
+                ),
+                Text('待评价')
+
+              ],
+            ),
+          ),
+          //------
         ],
       ),
     );
